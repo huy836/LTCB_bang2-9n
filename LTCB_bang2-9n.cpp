0df5b42f59e248ea1807e3dbb9896f857114d91b
ADMIN <ADMIN@LAB1-MAY07>
2024-03-11 15:36:36 +0700

    Add project files.

---
+++ LTCB_bang2-9n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	int so[8] = { 2,3,4,5,6,7,8,9 };
+
+	sort(so, so + 8, greater<int>());
+
+	for (int n = 1; n <= 10; n++) {
+		for (int i = 0; i < 8; i++) {
+			cout << so[i] << "x" << n << "=" <<
+				so[i] * n << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
